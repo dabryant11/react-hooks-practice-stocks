2eb342104e58b8784b6a72e542835f35a123cb08
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2021-02-28 17:08:33 -0500

    mmmmmm

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,12 +1,22 @@
 import React from "react";
 
-function Stock() {
+function Stock({ id, ticker, name, type, price }) {
+  function deleteStock() {
+    fetch(`http://localhost:3001/stocks/${id}`, {
+      method: "DELETE",
+    }).then((r) => r.json());
+  }
   return (
     <div>
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{ticker}</p>
+          <p className="card-text">{price}</p>
+          <p className="card-text">{type}</p>
+          <button onClick={deleteStock}>delete</button>
+          <br></br>
+          <br></br>
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocks }) {
+  const allStocks = stocks.map((stock) => {
+    return (
+      <Stock
+        key={stock.id}
+        id={stock.id}
+        ticker={stock.ticker}
+        name={stock.name}
+        type={stock.type}
+        price={stock.price}
+      />
+    );
+  });
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {allStocks}
     </div>
   );
 }
